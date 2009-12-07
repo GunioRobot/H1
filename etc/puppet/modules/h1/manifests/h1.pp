8d1e8a26af7f5133d60d56414652127c1db6807f
malcyL <malcolm@landonsonline.me.uk>
2009-12-07 16:28:31 +0000

    MAj-101 Added puppet files
    git-svn-id: http://mrpotatohead.talis.local/svn-repos/Services/H1/branches/ml_MAJ-101@57485 cddbaf53-7d3a-3f44-85ff-d5d6dcf41e07

--- etc/puppet/modules/h1/manifests/h1.pp
+++ etc/puppet/modules/h1/manifests/h1.pp
@@ -1,7 +1,7 @@
 # h1.pp
 
 class h1 {
-		include zookeeper
+                include zookeeper, java
         $h1dir = "h1"
     group { "h1":
                 name   => "h1",
@@ -18,38 +18,38 @@ class h1 {
                 group  => h1;
     }
         if $s3 == "true" {
-                s3get { "h1/$h1dist":
+                s3get { "h1/$h1Dist":
                         cwd     => "/opt/h1",
-                        name    => "$h1dist",
+                        name    => "$h1Dist",
                         expires => "900",
-                        before  => Exec["tar -zxf $h1dist"],
+                        before  => Exec["tar -zxf $h1Dist"],
                         bucket  => "talis-distros",
                         require => [ File["/opt/$h1dir"] ],
                 }
         } else {
         exec { "get-h1":
                     cwd     => "/opt/h1",
-                    creates => "/opt/h1/$h1dist",
+                    creates => "/opt/h1/$h1Dist",
                     path    => ["/usr/bin", "/usr/sbin"],
-                    before  => Exec["tar -zxf $h1dist"],
-                    command => "curl -s -f -o $h1dist http://$repoUrl/h1/$h1dist",
+                    before  => Exec["tar -zxf $h1Dist"],
+                    command => "curl -s -f -o $h1Dist http://$repoUrl/h1/$h1Dist",
                     require => [ File["/opt/$h1dir"] ],
             }
         }
-        exec { "tar -zxf $h1dist":
+        exec { "tar -zxf $h1Dist":
                 cwd     => "/opt/h1",
                 creates => "/opt/h1/lib",
-        onlyif  => "ls /opt/$h1dist",
+        onlyif  => "ls /opt/h1/$h1Dist",
                 path    => "/bin";
            "./install_h1_service.sh":
         cwd         => "/opt/$h1dir",
         creates     => "/etc/init.d/h1-node",
-        require     => Exec["tar -zxf $h1dist"],
-        path        => ["/bin"];
+        require     => Exec["tar -zxf $h1Dist"],
+        path        => ["/bin", "/sbin"];
         }
-    service { "h1" :
+    service { "h1-node" :
         ensure  => running,
         pattern => "h1",
-        require => [ File["/etc/init.d/h1-node"] ],
+        require => [ Exec["./install_h1_service.sh"], Class["java"] ],
     }
 }
