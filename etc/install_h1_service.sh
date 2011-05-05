c89284de79533ccb654488368ccc4c59de3f5b48
trickyf <rf@talisplatform.com>
2011-05-05 15:09:37 +0100

    Added jdeb to pom.xml, added support for debian systems for h1 install service

--- etc/install_h1_service.sh
+++ etc/install_h1_service.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 ## This script will install a H1 Node as a service
-cp h1-node.init.d /etc/init.d/h1-node
+cp /opt/h1/h1-node.init.d /etc/init.d/h1-node
 chmod 755 /etc/init.d/h1-node
 chown root /etc/init.d/h1-node
 chgrp root /etc/init.d/h1-node
-chkconfig --add h1-node
+if [ -a /etc/redhat-release ]
+then
+ chkconfig --add h1-node
+else
+ update-rc.d h1-node defaults
+fi
 exit
 
 
