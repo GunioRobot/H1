1d88353965a5d3d1561d4dfd442650d1616f15ba
Charles Care <c.p.care@gmail.com>
2011-06-30 16:11:29 +0100

    COH-138: Added http GET support to H1

---
+++ src/main/java/com/talis/platform/sequencing/NoSuchSequenceException.java
@@ -0,0 +1,10 @@
+package com.talis.platform.sequencing;
+
+
+public class NoSuchSequenceException extends SequencingException {
+
+	public NoSuchSequenceException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
